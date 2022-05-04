347f5305c5f0c38d97a6be32026611a6a6f4e125
Jean Buzzello <jeanbuzzello@gmail.com>
2022-05-03 22:15:33 -0300

    Trabalho 06

--- manutencao-carros/src/app/app.module.ts
+++ manutencao-carros/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Componente01Component } from './componente01/componente01.component';
 import { Componente02Component } from './componente02/componente02.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Componente01Component,
-    Componente02Component
+    Componente02Component,
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
