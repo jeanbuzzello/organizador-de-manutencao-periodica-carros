a9f3dc6bd82be85e759608cb38eb1786df7e2ea7
Jean Buzzello <jeanbuzzello@gmail.com>
2022-05-15 18:26:24 -0300

    Trabalho 07

--- manutencao-carros/src/app/app.module.ts
+++ manutencao-carros/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { Componente01Component } from './componente01/componente01.component';
 import { Componente02Component } from './componente02/componente02.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Componente03Component } from './componente03/componente03.component';
+import { Componente04Component } from './componente04/componente04.component';
+import { Componente05Component } from './componente05/componente05.component';
+import { Componente06Component } from './componente06/componente06.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,15 @@ import { RodapeComponent } from './rodape/rodape.component';
     Componente01Component,
     Componente02Component,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    Componente03Component,
+    Componente04Component,
+    Componente05Component,
+    Componente06Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ manutencao-carros/src/app/componente06/componente06.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Componente06Component } from './componente06.component';
+
+describe('Componente06Component', () => {
+  let component: Componente06Component;
+  let fixture: ComponentFixture<Componente06Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Componente06Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Componente06Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
